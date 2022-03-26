ebcf1c4a6af506d67f707bb6dcb8c4f8e36e5724
hbleao2 <99763976+hbleao2@users.noreply.github.com>
2022-03-26 08:17:45 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Button } from './components/Button';
 export default function App() {
   return (
     <div>
-      <h1>Softvaro</h1>
+      <h1>boilerplate</h1>
       <Button />
     </div>
   );
